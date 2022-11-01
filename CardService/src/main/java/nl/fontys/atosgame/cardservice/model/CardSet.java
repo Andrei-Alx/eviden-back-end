1bbdc9f26bfd8ac00c621c73e07f48cfdbfa8ef5
Brouwers,Eli E.S <eli.brouwers@student.fontys.nl>
2022-11-01 15:38:24 +0100

    Add CardSet model

---
+++ CardService/src/main/java/nl/fontys/atosgame/cardservice/model/CardSet.java
@@ -0,0 +1,36 @@
+package nl.fontys.atosgame.cardservice.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CardSet {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @JsonProperty
+    private UUID id;
+
+    @JsonProperty
+    private String name;
+
+    @JsonProperty
+    private String type;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(name = "card_set_cards",
+            joinColumns = @JoinColumn(name = "card_set_null"),
+            inverseJoinColumns = @JoinColumn(name = "cards_id"))
+    @JsonProperty
+    private Collection<Card> cards = new java.util.ArrayList<>();
+}
