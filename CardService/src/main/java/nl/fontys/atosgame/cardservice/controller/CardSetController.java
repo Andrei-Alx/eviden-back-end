28498b0a6e204de8faf1e7d64f183d13b782f2cd
Brouwers,Eli E.S <eli.brouwers@student.fontys.nl>
2022-11-02 09:38:50 +0100

    Add Cardset controller

---
+++ CardService/src/main/java/nl/fontys/atosgame/cardservice/controller/CardSetController.java
@@ -0,0 +1,74 @@
+package nl.fontys.atosgame.cardservice.controller;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import nl.fontys.atosgame.cardservice.dto.CreateCardDto;
+import nl.fontys.atosgame.cardservice.dto.CreateCardSetDto;
+import nl.fontys.atosgame.cardservice.model.Card;
+import nl.fontys.atosgame.cardservice.model.CardSet;
+import nl.fontys.atosgame.cardservice.service.CardService;
+import nl.fontys.atosgame.cardservice.service.CardSetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.UUID;
+
+/**
+ * Controller that handles all card set requests
+ * Performs CRUD operations on card sets
+ * @author Eli
+ */
+@RestController
+@RequestMapping("/api/cardsets")
+public class CardSetController {
+
+    private CardSetService cardSetService;
+
+    public CardSetController(@Autowired CardSetService cardSetService) {
+        this.cardSetService = cardSetService;
+    }
+
+    @PostMapping("/create")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cardset created", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = @Schema(implementation = CardSet.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @io.swagger.v3.oas.annotations.media.Content),
+    })
+    public ResponseEntity<CardSet> createCardSet(@RequestBody CreateCardSetDto createCardSetDto) {
+        try {
+            return ResponseEntity.ok(cardSetService.createCardSet(createCardSetDto));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/update")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cardset updated", content = @io.swagger.v3.oas.annotations.media.Content(mediaType = "application/json", schema = @Schema(implementation = CardSet.class))),
+            @ApiResponse(responseCode = "404", description = "Cardset not found", content = @io.swagger.v3.oas.annotations.media.Content),
+    })
+    public ResponseEntity<CardSet> updateCardSet(@RequestBody CardSet cardSet) {
+        try {
+            cardSetService.updateCardSet(cardSet);
+            return ResponseEntity.ok(cardSet);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cardset deleted", content = @io.swagger.v3.oas.annotations.media.Content),
+            @ApiResponse(responseCode = "404", description = "Cardset not found", content = @io.swagger.v3.oas.annotations.media.Content),
+    })
+    public ResponseEntity deleteCardSet(@PathVariable UUID id) {
+        try {
+            cardSetService.deleteCardSet(id);
+            return ResponseEntity.ok().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
