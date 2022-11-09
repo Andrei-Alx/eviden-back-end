83d3cd290fe6ebcf0f4271eb1ee3b5306e634f38
aniek234 <aniek234@gmail.com>
2022-11-09 15:49:01 +0100

    added skeleton of consumers gamecreated and gameended

--- LobbyService/src/main/java/nl/fontys/atosgame/lobbyservice/event/produced/LobbyCreatedEvent.java
+++ LobbyService/src/main/java/nl/fontys/atosgame/lobbyservice/event/produced/LobbyCreatedEvent.java
@@ -1,9 +1,10 @@
-package nl.fontys.atosgame.roundservice.event;
+package nl.fontys.atosgame.lobbyservice.event.produced;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import nl.fontys.atosgame.roundservice.model.Lobby;
+import nl.fontys.atosgame.lobbyservice.event.BaseEvent;
+import nl.fontys.atosgame.lobbyservice.model.Lobby;
 
 import java.util.UUID;
 
