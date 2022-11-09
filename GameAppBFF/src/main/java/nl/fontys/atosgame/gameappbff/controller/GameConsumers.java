d33d9ac1828889bb686eca72ec6fc1b2c68968a1
Brouwers,Eli E.S <eli.brouwers@student.fontys.nl>
2022-11-09 15:26:14 +0100

    Add Skeleton for C-29

--- GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/controller/GameConsumers.java
+++ GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/controller/GameConsumers.java
@@ -1,6 +1,7 @@
 package nl.fontys.atosgame.gameappbff.controller;
 
 import nl.fontys.atosgame.gameappbff.event.consumed.GameCreatedEvent;
+import nl.fontys.atosgame.gameappbff.event.consumed.GameStartedEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import java.util.function.Function;
 /**
  * Collection of consumers for game related events:
  * - GameCreatedEvent
+ * - GameStartedEvent
  * @author Eli
  */
 @Controller
@@ -23,6 +25,20 @@ public class GameConsumers {
      */
     @Bean
     public Function<Message<GameCreatedEvent>, Void> handleGameCreated() {
+        return message -> {
+            // TODO: implement
+            throw new UnsupportedOperationException("Not implemented yet");
+        };
+    }
+
+    /**
+     * Id: C-29
+     * Consumer for GameStartedEvent
+     * input topic: game-started-topic
+     * output topic: -
+     */
+    @Bean
+    public Function<Message<GameStartedEvent>, Void> handleGameStarted() {
         return message -> {
             // TODO: implement
             throw new UnsupportedOperationException("Not implemented yet");
