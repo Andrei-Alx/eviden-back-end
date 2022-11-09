60d6356f6d1a013b826d459da81638ee11e08922
Brouwers,Eli E.S <eli.brouwers@student.fontys.nl>
2022-11-09 13:33:34 +0100

    Add empty BFF projects

---
+++ AdminAppBFF/src/test/java/nl/fontys/atosgame/adminappbff/AdminAppBffApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.atosgame.adminappbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminAppBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
