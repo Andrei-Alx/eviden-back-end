0cdb47827b9f222d7bf5246d0b9828663bcc672d
Brouwers,Eli E.S <eli.brouwers@student.fontys.nl>
2022-11-10 14:03:30 +0100

    Add websocket test code that works

--- GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/controller/LobbySocketController.java
+++ GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/controller/LobbySocketController.java
@@ -1,13 +1,19 @@
 package nl.fontys.atosgame.gameappbff.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class LobbySocketController {
 
-    @SendTo("/socket/gameapp/lobby/playerJoined")
-    public String playerJoined(String message) {
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    public String playerJoined(String lobbyId, String message) {
+        template.convertAndSend(String.format("/socket/gameapp/%s/playerJoined", lobbyId), message);
         return message;
     }
 }
--- GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/controller/TestController.java
+++ GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/controller/TestController.java
@@ -2,6 +2,7 @@ package nl.fontys.atosgame.gameappbff.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,8 @@ public class TestController {
     private LobbySocketController lobbySocketController;
 
     @PostMapping("/test")
-    public String test() {
-        lobbySocketController.playerJoined("Test over websocket");
+    public String test(@RequestBody String lobbyId) {
+        lobbySocketController.playerJoined(lobbyId,"Test over websocket");
         return "test";
     }
 }
