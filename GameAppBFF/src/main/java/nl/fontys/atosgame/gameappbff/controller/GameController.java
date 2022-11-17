5e1b643c2c3b02323ff54cdfec6afccbbe24fe87
Kevin <i439586@fhict.nl>
2022-11-17 15:55:46 +0100

    added comments and swagger

--- GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/controller/GameController.java
+++ GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/controller/GameController.java
@@ -1,5 +1,7 @@
 package nl.fontys.atosgame.gameappbff.controller;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import nl.fontys.atosgame.gameappbff.dto.GameResponseDto;
 import nl.fontys.atosgame.gameappbff.dto.LobbyResponseDto;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,7 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/gameappbff")
 public class GameController {
 
+    /**
+     * Getting all games.
+     *
+     * @return all games.
+     */
     @GetMapping("/games")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Getting all the games"),
+                    @ApiResponse(responseCode = "404", description = "No games found"),
+            }
+    )
     public GameResponseDto getGames() {
         LobbyResponseDto lobby = new LobbyResponseDto();
         lobby.setId("1");
