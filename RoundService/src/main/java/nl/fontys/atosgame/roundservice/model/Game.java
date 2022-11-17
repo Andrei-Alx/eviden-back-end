44256a91b31e4215a306a39f4cd7d7a7bce5fa3b
Brouwers,Eli E.S <eli.brouwers@student.fontys.nl>
2022-11-17 23:25:11 +0100

    Add logic to get currently active round of a game

--- RoundService/src/main/java/nl/fontys/atosgame/roundservice/model/Game.java
+++ RoundService/src/main/java/nl/fontys/atosgame/roundservice/model/Game.java
@@ -3,12 +3,14 @@ package nl.fontys.atosgame.roundservice.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import javax.persistence.*;
 import jdk.jfr.DataAmount;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nl.fontys.atosgame.roundservice.enums.RoundStatus;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 import org.hibernate.annotations.Type;
@@ -33,4 +35,17 @@ public class Game {
     @JoinColumn(name = "lobby_id")
     @JsonProperty
     private Lobby lobby;
+
+    /**
+     * Get the current active round
+     * @return An optional containing the current active round or an empty optional if no round is active
+     */
+    public Optional<Round> getCurrentRound() {
+        for (Round round : rounds) {
+            if (round.getStatus() == RoundStatus.IN_PROGRESS) {
+                return Optional.of(round);
+            }
+        }
+        return Optional.empty();
+    }
 }
