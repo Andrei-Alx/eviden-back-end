9d15dc2bc59a8e5bf340e80749a563bb880597b6
Brouwers,Eli E.S <eli.brouwers@student.fontys.nl>
2022-11-21 12:36:15 +0100

    Change RoundCreatedEvent to include gameId

--- RoundService/src/main/java/nl/fontys/atosgame/roundservice/repository/PlayerRoundRepository.java
+++ RoundService/src/main/java/nl/fontys/atosgame/roundservice/repository/PlayerRoundRepository.java
@@ -4,15 +4,9 @@ import java.util.UUID;
 import nl.fontys.atosgame.roundservice.model.PlayerRound;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-<<<<<<< Updated upstream
-=======
-import java.util.UUID;
-
 /**
  * Repository for player rounds related operations
  * @author Eli
  */
->>>>>>> Stashed changes
 @Repository
 public interface PlayerRoundRepository extends JpaRepository<PlayerRound, UUID> {}
