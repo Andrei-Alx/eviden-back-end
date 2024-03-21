fdb5a16af091360ecafe29305d0e1cc4ed632598
Smeef <freek.jansen00@hotmail.com>
2024-03-21 11:02:40 +0100

    Code Cleanup Second Pass
    Removed Redundant CarSetEventService

--- GameAppBFF/src/main/java/nl/fontys/atosgame/gameappbff/service/CardSetEventService.java
+++
@@ -1,9 +0,0 @@
-package nl.fontys.atosgame.gameappbff.service;
-
-import nl.fontys.atosgame.gameappbff.event.produced.CardSetRequestEvent;
-import org.springframework.messaging.Message;
-
-public interface CardSetEventService {
-
-    Message<CardSetRequestEvent> cardSetRequest();
-}
