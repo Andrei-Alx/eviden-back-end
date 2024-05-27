8fb6e97070b66248c880cdaf90ec666db17aa324
Fabian te Raaij <f.teraaij@student.fontys.nl>
2024-05-27 19:58:13 +0200

    EVIDEN-25 Added ingress and config to project

--- deployment/operator-setup.sh
+++ deployment/operator-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Add NGINX ingress controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
 # Deploy CRD definitions used by the MySQL operator
 kubectl apply -f https://raw.githubusercontent.com/mysql/mysql-operator/trunk/deploy/deploy-crds.yaml
 # Deploy the MySQL operator for K8, which includes RBAC definitions.
