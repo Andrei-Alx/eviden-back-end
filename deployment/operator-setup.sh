a386769ab939f291f2181dc845b925a6aa951552
Fabian te Raaij <f.teraaij@student.fontys.nl>
2024-06-05 13:22:09 +0200

    EVIDEN-25 Add custom kafka deployment

--- deployment/operator-setup.sh
+++ deployment/operator-setup.sh
@@ -7,7 +7,5 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main
 kubectl apply -f https://raw.githubusercontent.com/mysql/mysql-operator/trunk/deploy/deploy-crds.yaml
 # Deploy the MySQL operator for K8, which includes RBAC definitions.
 kubectl apply -f https://raw.githubusercontent.com/mysql/mysql-operator/trunk/deploy/deploy-operator.yaml
-
+# Add Kafka cluster operator
 kubectl create -f 'https://strimzi.io/install/latest?namespace=default' -n default
-# Apply the `Kafka` Cluster CR file
-kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent-single.yaml -n default
